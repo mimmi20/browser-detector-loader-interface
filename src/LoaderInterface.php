45e2573ca9f6a30b34965a0b73f60e12607d1298
Thomas Müller <mimmi20@live.de>
2018-12-05 20:08:30 +0100

    make exception final

--- src/LoaderInterface.php
+++ src/LoaderInterface.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the browser-detector-loader-interface package.
  *
- * Copyright (c) 2015-2017, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2015-2018, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,15 +11,6 @@
 declare(strict_types = 1);
 namespace BrowserDetector\Loader;
 
-/**
- * Browser detection class
- *
- * @category  BrowserDetector
- *
- * @author    Thomas Mueller <mimmi20@live.de>
- * @copyright 2012-2016 Thomas Mueller
- * @license   http://www.opensource.org/licenses/MIT MIT License
- */
 interface LoaderInterface
 {
     /**
