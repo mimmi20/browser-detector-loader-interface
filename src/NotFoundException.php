146f60ac99476b6beac0cf941a340e6455e5fe94
Thomas Müller <mimmi20@live.de>
2018-12-05 20:08:30 +0100

    make exception final

--- src/NotFoundException.php
+++ src/NotFoundException.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the browser-detector-loader-interface package.
  *
- * Copyright (c) 2015-2017, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2015-2018, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,16 +11,7 @@
 declare(strict_types = 1);
 namespace BrowserDetector\Loader;
 
-/**
- * an exception thrown if data were not found
- *
- * @category  BrowserDetector
- *
- * @author    Thomas Mueller <mimmi20@live.de>
- * @copyright 2012-2016 Thomas Mueller
- * @license   http://www.opensource.org/licenses/MIT MIT License
- */
-class NotFoundException extends \UnexpectedValueException
+final class NotFoundException extends \UnexpectedValueException
 {
     // nothing to do here
 }
